ce37331599a4f5db81779102b2ef1fc1c45ffb4f
Antoine1989 <antoine-guillou@orange.fr>
2022-12-30 00:22:38 +0100

    ajout fontAwesome et accordion

--- src/Composants/ListSpots.js
+++ src/Composants/ListSpots.js
@@ -1,10 +1,12 @@
 //import 'bootsrap/dist/css/bootsrap.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container,Card,Col,ListGroup} from 'react-bootstrap';
+import {Container,Card,Col,ListGroup,Accordion} from 'react-bootstrap';
 import FishinService from '../Service/FishinService';
 import React, {Component,useState,useEffect} from 'react'
 import './ListSpots.css'
 import ListCaptures from './ListCaptures';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnchor, faFish } from '@fortawesome/free-solid-svg-icons';
 
 
 function ListSpots() {
@@ -31,20 +33,27 @@ function ListSpots() {
     { spots.map(
         spot=>
         <div key={spot.id}>
-        <Container className='p-4'>
+        <Container className='p-4 '>
         <Col md="4">
        
-       
-                  
             <ListGroup variant="flush">
                 <ListGroup.Item>
               
-                <Card className="border-white">
-                <Card.Header className="entete">{spot.nomSpot} {spot.ville}</Card.Header>
-                <Card.Body >
+                <Card className="border-white ">
+                <Accordion >
                     
+                
+        
+                <Accordion.Header style={{ backgroundColor: '#07E79D' }}>{spot.nomSpot} <FontAwesomeIcon icon={faAnchor} />{/*{spot.ville}*/}</Accordion.Header>
+               {/* <Card.Body >*/}
+                <Accordion.Body>
+                <FontAwesomeIcon icon={faFish} />
+                <FontAwesomeIcon icon={faFish} />
                     <ListCaptures spot={spot.id}/>
-                </Card.Body>
+                </Accordion.Body>
+               {/* </Card.Body>*/}
+               
+               </Accordion>
                 
                 </Card>
         
